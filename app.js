55136dfb70ec2697af3b17fbf53782cb24132afe
Ronella Dias <ronelladias17@gmail.com>
2026-02-18 18:13:51 +0530

    write to file using fs.writeFile

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+fs.writeFile(
+  "file.txt",
+  "Hi I'm writing to a file",
+  "utf8",
+  function (err, data) {
+    if (err) throw err;
+    console.log("File has been written");
+  },
+);
