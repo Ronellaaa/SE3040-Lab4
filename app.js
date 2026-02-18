951d1aa450fe243e5cf47e38bb2e19f5566dc8ea
Ronella Dias <ronelladias17@gmail.com>
2026-02-18 18:20:21 +0530

    create simple HTTP server on port 8080

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ fs.writeFile(
     console.log("File has been written");
   },
 );
+
+const http = require("http");
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World !!!!");
+    res.end();
+  })
+  .listen(8080);
